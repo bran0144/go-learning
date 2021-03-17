efe6d5fe135df8a3e87c469ea9925db89d7bddd2
Katherine Gott <katherinegott@Katherines-MacBook-Pro.local>
2021-03-17 17:44:25 -0500

    first go program

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/pluralsight/webservice
+
+go 1.16
+
+require golang.org/x/tools/gopls v0.6.8 // indirect
---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"github.com/pluralsight/webservice/models"
+)
+func main() {
+	u := models.User{
+		ID: 2,
+		FirstName: "Ali",
+		LastName: "McMillian",
+	}
+	fmt.Println(u)
+}
